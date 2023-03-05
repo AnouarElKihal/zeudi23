cf083a303872918186cbaf3850ae47ab7d0c71db
elkihalanouar <68613907+AnouarElKihal@users.noreply.github.com>
2023-03-05 15:28:51 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+window.addEventListener("scroll", function() {
+    const nav = document.querySelector("nav");
+    if (window.pageYOffset > 0) {
+    nav.classList.add("scrolled");
+    } else {
+        nav.classList.remove("scrolled");
+    }
+});
+
+$(document).ready(function() {
+    $(".navbar a").on("click", function() {
+      $(".navbar").removeClass("open");
+      $("#check").prop("checked", false);
+      var target = $(this).attr("href");
+      $("html, body").animate({
+        scrollTop: $(target).offset().top
+      }, 1000);
+    });
+});
+
+var player = new Video.Player('video-player', {
+    playsinline: true
+});
+  
+playsinline.init();
